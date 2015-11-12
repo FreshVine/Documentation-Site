08b430348b2f2409827b8c3bc3bb4776889aa66b
Paul Prins <paulprins@gmail.com>
2015-11-12 16:39:45 -0600

    Adapt youtube for embedded playlists and videos

--- templates/helpers/youtube.js
+++ templates/helpers/youtube.js
@@ -1,5 +1,7 @@
 
-
+// API Docs for the youtube embeded player - https://developers.google.com/youtube/player_parameters?hl=en
+// use with {{{youtube 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}}}
+// Link can either be to a video, or the embed link for a video or playlist. Attributes to exclude related videos already included
 module.exports = function(link) {
 	//
 	// Helper functions for parsing URLs into their parts
@@ -43,24 +45,33 @@ module.exports = function(link) {
     for( var i = 0; i < hashes.length; i++ ){
         hash = hashes[i].split('=');
 		if( hash[0] == 'v' ){
-			embed = hash[1];
+			embed = '//www.youtube.com/embed/' + hash[1];
 		}else{
-	        vars.push(hash[0]);
-	        vars[hash[0]] = hash[0] + '=' + hash[1];
+	        vars.push(hash[0] + '=' + hash[1]);
 		}
     }
+	if( link.indexOf('/embed') > 0 ){
+		embed = '//www.youtube.com' + parser.path;
+	}
+
+	vars.push('rel=0');
+	// vars['rel'] = 'rel=0';	// Disable the related videos at end of play
+	vars.push('modestbranding=1');
+	// vars['modestbranding'] = 'modestbranding=1';	// Turn off youtube branding
+	// Build the embed link
+	//
 
 	if( typeof embed == 'undefined' ){
 		return '<div class="iframe-wrapper">YouTube link not correctly formed<br />It must include the <em>?v=XXXXXX</em> attribtue. Link included:<br />' + link + '</div>';
 	}
 
 	if( vars.length > 0 ){
-		embedAppend = '?' . vars.join('&');
+		embedAppend = '?' + vars.join('&');
 	}
 
 
 	var r = 
-		'<div class="iframe-wrapper"><iframe src="//www.youtube.com/embed/'
+		'<div class="iframe-wrapper"><iframe src="'
 		+ embed
 		+ embedAppend
 		+ '" width="560" height="315" frameborder="0" allowfullscreen></iframe></div>';
